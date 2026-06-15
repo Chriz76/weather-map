053624edb9ab3d990ae06f77692b17030524865d
Chriz <48691511+Chriz76@users.noreply.github.com>
2026-06-15 23:08:10 +0200

    Add link to documentation in logo widget

--- js/controls/logoWidget.js
+++ js/controls/logoWidget.js
@@ -7,13 +7,15 @@ export function registerLogoControl(map) {
       L.DomEvent.disableScrollPropagation(container);
 
       container.innerHTML = `
-        <div class="logo-container">
-          <img class="logo-icon" src="favicon.svg" alt="ICON-D2 RUC logo" />
-          <div class="logo-text">
-            <h1>ICON-D2 RUC</h1>
-            <h2>The Only Hourly Updated Forecast</h2>
+        <a href="./info.html" class="logo-link" title="Dokumentation und Projekt-Informationen anzeigen">
+          <div class="logo-container">
+            <img class="logo-icon" src="favicon.svg" alt="ICON-D2 RUC Wetterkarte Logo" />
+            <div class="logo-text">
+              <h1>ICON-D2 RUC</h1>
+              <h2>The Only Hourly Updated Forecast</h2>
+            </div>
           </div>
-        </div>
+        </a>
       `;
 
       return container;
