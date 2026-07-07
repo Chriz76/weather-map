c92864203afe33a3a98c088bdc22f5d5b0f7c183
Gonk <chris@zink.tv>
2026-07-07 22:19:12 +0200

    bump app version

--- js/config.js
+++ js/config.js
@@ -1,6 +1,6 @@
 export const BASE_URL = "https://winddata.pages.dev/";
 
-export const EXPECTED_API_VERSION = "1.0.0"; // Deine gewünschte Version
+export const EXPECTED_API_VERSION = "1.1.0"; // Deine gewünschte Version
 
 export const GRID_CELL_SIZE = 1.0;
 
